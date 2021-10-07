84abf0da12abcc09881fd1dc8d129302d3a5f307
cviougeat <cviougeat@gmail.com>
2021-10-07 20:20:44 +0200

    Ajout des entités Reservation et Comment et de leurs CRUD migration et màj de la BDD

---
+++ src/Entity/Comment.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $CommentId;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $Content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $ReservationIdComment;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommentId(): ?int
+    {
+        return $this->CommentId;
+    }
+
+    public function setCommentId(int $CommentId): self
+    {
+        $this->CommentId = $CommentId;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->Content;
+    }
+
+    public function setContent(string $Content): self
+    {
+        $this->Content = $Content;
+
+        return $this;
+    }
+
+    public function getCreated(): ?\DateTimeInterface
+    {
+        return $this->created;
+    }
+
+    public function setCreated(\DateTimeInterface $created): self
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getReservationIdComment(): ?int
+    {
+        return $this->ReservationIdComment;
+    }
+
+    public function setReservationIdComment(int $ReservationIdComment): self
+    {
+        $this->ReservationIdComment = $ReservationIdComment;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Reservation.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ReservationRepository::class)
+ */
+class Reservation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $ReservationId;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $ReservationAdress;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $NumberGuests;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $StartDate;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $EndDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $HostName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $GuestName;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $NumberNights;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $PaymentTotal;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getReservationId(): ?int
+    {
+        return $this->ReservationId;
+    }
+
+    public function setReservationId(int $ReservationId): self
+    {
+        $this->ReservationId = $ReservationId;
+
+        return $this;
+    }
+
+    public function getReservationAdress(): ?string
+    {
+        return $this->ReservationAdress;
+    }
+
+    public function setReservationAdress(string $ReservationAdress): self
+    {
+        $this->ReservationAdress = $ReservationAdress;
+
+        return $this;
+    }
+
+    public function getNumberGuests(): ?int
+    {
+        return $this->NumberGuests;
+    }
+
+    public function setNumberGuests(int $NumberGuests): self
+    {
+        $this->NumberGuests = $NumberGuests;
+
+        return $this;
+    }
+
+    public function getStartDate(): ?\DateTimeInterface
+    {
+        return $this->StartDate;
+    }
+
+    public function setStartDate(\DateTimeInterface $StartDate): self
+    {
+        $this->StartDate = $StartDate;
+
+        return $this;
+    }
+
+    public function getEndDate(): ?\DateTimeInterface
+    {
+        return $this->EndDate;
+    }
+
+    public function setEndDate(\DateTimeInterface $EndDate): self
+    {
+        $this->EndDate = $EndDate;
+
+        return $this;
+    }
+
+    public function getHostName(): ?string
+    {
+        return $this->HostName;
+    }
+
+    public function setHostName(string $HostName): self
+    {
+        $this->HostName = $HostName;
+
+        return $this;
+    }
+
+    public function getGuestName(): ?string
+    {
+        return $this->GuestName;
+    }
+
+    public function setGuestName(string $GuestName): self
+    {
+        $this->GuestName = $GuestName;
+
+        return $this;
+    }
+
+    public function getNumberNights(): ?int
+    {
+        return $this->NumberNights;
+    }
+
+    public function setNumberNights(int $NumberNights): self
+    {
+        $this->NumberNights = $NumberNights;
+
+        return $this;
+    }
+
+    public function getPaymentTotal(): ?float
+    {
+        return $this->PaymentTotal;
+    }
+
+    public function setPaymentTotal(float $PaymentTotal): self
+    {
+        $this->PaymentTotal = $PaymentTotal;
+
+        return $this;
+    }
+}
